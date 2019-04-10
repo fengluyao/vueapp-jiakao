ff9f82c0970535c953666b054dd159d75c82aba9
fengluyao <44237172+fengluyao@users.noreply.github.com>
2019-04-10 18:28:32 +0800

    第一次

---
+++ app/src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+    <el-container>
+        <el-header class="header">
+            <el-row :gutter="20">
+                <el-col :span="6" :offset="1">
+                    <div class="grid-content">
+                        <img src="https://m.ixbest.com/addons/zjhj_mall/core/web/uploads/image/b9/b987f74259e0c263017481bddf6a8df998d72801.jpg" alt="" class="logo" @click="aclick()">    
+                    </div>
+                </el-col>
+                <el-col :span="10">
+                    <div class="grid-content title" v-text="title"></div>
+                </el-col>
+                <el-col :span="3">
+                    <div class="grid-content">
+                        <img src="https://m.ixbest.com/addons/zjhj_mall/core/web/uploads/image/54/541dd6bca07899f7f6621edb342bd398be6b511e.jpg" alt="" class="right">
+                    </div>
+                </el-col>
+                <el-col :span="4">
+                    <div class="grid-content">
+                        <img src="https://m.ixbest.com/addons/zjhj_mall/core/web/uploads/image/0b/0bdb80263039e523e0fb1767cb6f25dcd6b33e0b.jpg" alt="" class="right">
+                    </div>
+                </el-col>
+            </el-row>
+        </el-header>
+    </el-container>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            title : "驾考神器"
+        }
+    },
+    methods:{
+        aclick(){
+            this.$router.push({name:'HelloWorld'});
+        }
+    }
+}
+</script>
+<style>
+    .header{
+        background: #20AE88;
+        height: 200px !important;
+    }
+    .grid-content {
+        border-radius: 10px;
+        height: 200px;
+    }
+    .logo{
+        width:auto;
+        height:90%;
+        margin-top: 3%;
+    }
+    .right{
+        width: auto;
+        height: 100%;
+    }
+    .title{
+        color: #fff;
+        font-size: 100px;
+        font-weight: bold;
+        text-align: center;
+        line-height: 200px;
+    }
+</style>
---
+++ app/src/components/Index.vue
@@ -0,0 +1,113 @@
+<template>
+    <div id="app">
+        <Header></Header>
+        <div class="sudoku_row"  >
+            <div class="sudoku_item " v-for="(sudoku,index) in sudokus" :key="index" >
+                {{sudoku.name}}
+            </div>
+        </div>
+        <el-card class="box-card" shadow="always">
+            <div slot="header" class="clearfix">
+                <span>最新资讯</span>
+                <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+            </div>
+            <div v-for="(index,item) in zixun" :key="item.id" class="text item">
+                {{index.text}}
+            </div>
+        </el-card>
+        <div class="body">
+            <div class="btn">
+                <el-button type="success" class="button">科目一考试</el-button>
+                <el-button type="danger" class="button">科目四考试</el-button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Header from "@/components/Header";
+export default {
+    components:{Header},
+    name: 'sudoku',
+    data() {  
+        return {  
+        sudokus:[{id:0,name:'驾考资讯'},
+                 {id:1,name:'驾考宝典'},
+                 {id:2,name:'驾考视频'},
+                 {id:3,name:'交通标识'},
+                 {id:4,name:'科目一考试'},
+                 {id:5,name:'科目四考试'},
+                 {id:6,name:'科目二学习'},
+                 {id:7,name:'科目三学习'}
+                ],
+        zixun:[{id:0,text:'杭州最新驾考资讯杭州最新驾考资讯杭州最新驾考资讯杭州最新驾考资讯'},
+               {id:1,text:'杭州最新驾考资讯杭州最新驾考资讯杭州最新驾考资讯杭州最新驾考资讯'},
+              ]
+        };  
+    },
+}
+</script>
+<style>
+    body{
+        background: #ddd;
+    }
+    .sudoku_row{
+        display: flex;
+        align-items: center;
+        width:100%;
+        flex-wrap: wrap;
+    }
+    .sudoku_item{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width:25%;
+        height: 120px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        font-size: 30px;
+        background: #666;
+        color: #fff;
+        border: 1px solid #999;
+    }
+    .text {
+        font-size: 14px;
+    }
+
+    .item {
+        width: 100%;
+        height: 200px;
+        margin-bottom: 18px;
+        border-bottom: 1px solid #ddd;
+    }
+
+    .clearfix:before,
+    .clearfix:after {
+        display: table;
+        content: "";
+    }
+    .clearfix:after {
+        clear: both
+    }
+
+    .box-card {
+        width: 98%;
+        margin: 30px auto;
+    }
+    .body{
+        width: 100%;
+        height: auto;
+        background: #fff;
+    }
+    .btn{
+        width: 100%;
+        height: 150px;
+        display: flex;
+        flex-direction: row;
+    }
+    .button{
+        width: 47%;
+        height: 80%;
+        margin: 11px auto;
+    }
+</style>
